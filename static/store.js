918a3130e79d4a2b6526b0fef631ac2b947066d6
Evgeniy Baranov <master@kompolom.ru>
2026-01-31 10:53:00 +0300

    Add Store Class for Historical Events

---
+++ static/store.js
@@ -0,0 +1,71 @@
+import {
+  from,
+  filter,
+  tap,
+  toArray,
+  map,
+  switchMap,
+  combineLatest,
+  startWith,
+} from "https://cdn.jsdelivr.net/npm/rxjs@7.8.2/+esm";
+import { HistoricalEvent } from "./entities/historical-event.js";
+import { loadData } from "./wikibase.js";
+
+export class Store {
+  #eventsById;
+  #loadedScapes;
+  constructor() {
+    this.#eventsById = new Map();
+    this.#loadedScapes = new Map();
+  }
+
+  /**
+   *
+   * @param {HistoricalEvent[]} events
+   */
+  addEvents(events) {
+    for (const event of events) {
+      this.#eventsById.set(event.id, event);
+    }
+  }
+
+  /**
+   * Find single event by wikidata id
+   * @param {string} eventId
+   * @returns {HistoricalEvent|null}
+   */
+  getEventById(id) {
+    return this.#eventsById.get(id);
+  }
+
+  /**
+   *
+   * @param {import('./value-objects/iso-range').ISODateRange} dateRange
+   * @param {import('./value-objects/bbox').BBox} bbox
+   * @returns {Observable<HistoricalEvent>}
+   */
+  getEvents(dateRange, bbox) {
+    console.debug("loading events:", String(dateRange));
+    return from(this.#eventsById.values()).pipe(
+      tap((e) => console.debug("raw", e)),
+      filter(
+        (event) =>
+          HistoricalEvent.inPeriod(dateRange, event) &&
+          HistoricalEvent.inPlace(bbox, event),
+      ),
+      toArray(),
+      tap((e) => console.debug("filtered", e)),
+      switchMap((arr) => {
+        if (arr.length) {
+          return from(arr);
+        }
+        console.debug("Load from wikidata");
+        return loadData({ range: dateRange, bbox, limit: 500 }).pipe(
+          map((events) => events.map(HistoricalEvent.create)),
+          tap((he) => this.addEvents(he)),
+          switchMap((he) => from(he)),
+        );
+      }),
+    );
+  }
+}
